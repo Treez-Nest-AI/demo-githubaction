1c35f680aabb0b370102bc9a8a1fac7d91e6708d
Treez-Nest-AI <technical@treezenterprise.com>
2025-11-25 15:57:51 +0530

    delete handler.py

--- services/python-demo/tests/test_handler.py
+++ services/python-demo/tests/test_handler.py
@@ -1,9 +1,9 @@
 from handler import hello
 
 def test_status_code():
-    response = hello()
+    response = hello({}, {})
     assert response['statusCode'] == 200
 
 def test_message():
-    response = hello()
+    response = hello({}, {})
     assert response['body'] == "Hello from python demo service!"
