18888b2ad74acf4d4ecca366b44d8abaa48a1e31
Treez-Nest-AI <technical@treezenterprise.com>
2025-11-26 17:17:24 +0530

    add gitleaks

--- services/python-demo/handler.py
+++ services/python-demo/handler.py
@@ -1,6 +1,6 @@
 def hello(event, context):
     # ❗ FAKE AWS KEY (This will make Gitleaks fail)
-    AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+    AWS_ACCESS_KEY_ID = "AKIA1234567890ABCD1234"
     return {
         "statusCode": 200,
         "body": "Hello from python demo service!"
